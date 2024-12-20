9a65161d0a0dc4602379246d68e154f7cdc1be65
User <User@LAPTOP-7HE0MFR7>
2024-12-20 17:01:23 +0100

    Prima implementazone dell'interfaccia della chat.(Starter della chat)

---
+++ src/main/java/clientChat/Client.java
@@ -0,0 +1,11 @@
+
+package clientChat;
+
+
+public class Client {
+    public static void main(String[] args) {
+        ClientStart clientStart = new ClientStart();
+        
+        clientStart.setVisible(true);
+    }
+}
---
+++ src/main/java/clientChat/ClientStart.java
@@ -0,0 +1,152 @@
+package clientChat;
+
+public class ClientStart extends javax.swing.JFrame {
+
+    public ClientStart() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lblClientChat = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtIp = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        txtPort = new javax.swing.JTextField();
+        btnConnection = new javax.swing.JToggleButton();
+        lblShowErrors = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("La Chat");
+
+        lblClientChat.setBackground(new java.awt.Color(204, 204, 255));
+        lblClientChat.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lblClientChat.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblClientChat.setText("Client Della Chat");
+
+        jLabel2.setBackground(new java.awt.Color(153, 153, 255));
+        jLabel2.setText("IP:");
+
+        txtIp.setBackground(new java.awt.Color(204, 204, 255));
+        txtIp.setText("(inserito di default)");
+        txtIp.setCaretColor(new java.awt.Color(255, 255, 255));
+
+        jLabel3.setBackground(new java.awt.Color(153, 153, 255));
+        jLabel3.setText("Porta:");
+
+        txtPort.setBackground(new java.awt.Color(204, 204, 255));
+        txtPort.setText("(inserito dall'utente)");
+        txtPort.setCaretColor(new java.awt.Color(255, 255, 255));
+
+        btnConnection.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btnConnection.setText("CONNETTI ALLA CHAT");
+
+        lblShowErrors.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblShowErrors.setText("(spazio per eventali errori)");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(39, 39, 39)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(txtIp, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(35, 35, 35)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtPort, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(btnConnection))))
+                        .addGap(0, 68, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblClientChat, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblShowErrors, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(lblClientChat, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtIp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(39, 39, 39)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtPort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(140, 140, 140)
+                .addComponent(btnConnection, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(lblShowErrors, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(40, Short.MAX_VALUE))
+        );
+
+        txtIp.getAccessibleContext().setAccessibleName("IpClient");
+        txtPort.getAccessibleContext().setAccessibleName("PortClient");
+        btnConnection.getAccessibleContext().setAccessibleName("ConnettiB1");
+        lblShowErrors.getAccessibleContext().setAccessibleName("ErrorLabel1");
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ClientStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ClientStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ClientStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ClientStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ClientStart().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton btnConnection;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel lblClientChat;
+    private javax.swing.JLabel lblShowErrors;
+    private javax.swing.JTextField txtIp;
+    private javax.swing.JTextField txtPort;
+    // End of variables declaration//GEN-END:variables
+}
