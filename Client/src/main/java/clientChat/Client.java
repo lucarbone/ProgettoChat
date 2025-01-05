1fdde9e31112eb7113da998f8674fc11425daab2
Luca Carbone <147904386+lucarbone@users.noreply.github.com>
2025-01-05 13:13:29 +0100

    Delete Client directory

--- Client/src/main/java/clientChat/Client.java
+++
@@ -1,9 +0,0 @@
-
-package clientChat;
-
-public class Client {
-    public static void main(String[] args) {
-        ClientStart clientStart = new ClientStart();
-        clientStart.setVisible(true);
-    }
-}
